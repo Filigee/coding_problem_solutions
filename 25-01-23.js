e14f2ad4c498471bfeeee9703095e319c78420b2
Bradley Hagger <brad_2nd@hotmail.com>
2023-01-25 17:14:35 +0000

    solution one

--- 25-01-23.js
+++ 25-01-23.js
@@ -1,7 +1,9 @@
 // https://www.codewars.com/kata/526dbd6c8c0eb53254000110/train/javascript
 
 function alphanumeric(string){
-    const regex = /[^\p{sc=Latin}\p{N}]/gu
-    
-    return string.match(regex) == null
-  }
+  if(string == "")
+    return false
+  
+  const regex = /[^\p{sc=Latin}\p{N}]/gu
+  return string.match(regex) == null
+}
