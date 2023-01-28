c0b40626029bf154d26d42be7c448d5fd462b6db
Bradley Hagger <brad_2nd@hotmail.com>
2023-01-28 11:19:48 +0000

    Solution: 21 Sticks

---
+++ 7kyu/21_sticks.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/5866a58b9cbc02c4f8000cac
+
+function makeMove(sticks) {
+  if(sticks == 21)
+    return 1
+  
+  return amountOfSticks(sticks)
+}
+
+function amountOfSticks(sticks){
+  if((sticks - 3) % 4 == 0) return 3
+  else if((sticks - 2) % 4 == 0) return 2
+  else return 1
+}
