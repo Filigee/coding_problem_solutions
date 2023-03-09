895a84df2f5500b07819c6f2a0c4d4658ac726a6
Bradley Hagger <brad_2nd@hotmail.com>
2023-03-09 11:24:26 +0000

    Retry

--- retry.js
+++ retry.js
@@ -1,3 +1,5 @@
 // https://www.codewars.com/kata/58402cdc5225619d0c0000cb/train/javascript
 
 // https://www.codewars.com/kata/5840df348c7e72243100012a
+
+// https://www.codewars.com/kata/626d96eb49cb3c7a2f634bbf/javascript
