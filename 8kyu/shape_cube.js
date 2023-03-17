3b79f32652b57a767c45f6a33c92d8daa45feb09
Bradley Hagger <brad_2nd@hotmail.com>
2023-03-17 20:57:54 +0000

    Solution: Shape Cube

--- 8kyu/shape_cube.js
+++ 8kyu/shape_cube.js
@@ -1,9 +1,6 @@
 // https://www.codewars.com/kata/58d248c7012397a81800005c
 
 var cubeChecker = function(volume, side){
-    // number
-    // bool
-    
     if(side == 0 || side > volume)
       return false
     
