954f834fd89ce47421fab181f7bb3fd39f14f3bf
Bradley Hagger <brad_2nd@hotmail.com>
2023-03-23 08:35:12 +0000

    Solution: Sum of Sequences

---
+++ 7kyu/sum_of_seq.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/586f6741c66d18c22800010a
+
+const sequenceSum = (begin, end, step) => {
+    let sum = 0
+    
+    for(let i = begin; i <= end; i += step){
+      sum += i
+    }
+    
+    return sum
+}
