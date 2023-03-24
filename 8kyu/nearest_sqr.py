97baf749d3a989595ce09726e66767c8b6fff18d
Bradley Hagger <brad_2nd@hotmail.com>
2023-03-24 11:40:42 +0000

    Solution: Nearest Sqr

---
+++ 8kyu/nearest_sqr.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/5a805d8cafa10f8b930005ba/train/python
+
+import math
+
+def nearest_sq(n):
+    # find the nearest square number of n
+    
+    return round(math.sqrt(n)) ** 2
+    
