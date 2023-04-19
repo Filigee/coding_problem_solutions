d1f7f1714b1d762ba9635b86e94c42c0a86887bf
Bradley Hagger <brad_2nd@hotmail.com>
2023-04-19 09:54:16 +0100

    Solution: Messi Goals

---
+++ 8kyu/messi_goals.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/javascript
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+  }
