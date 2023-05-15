c6c5364069cbd8d633c6635be6c1c95e067f6277
Bradley Hagger <brad_2nd@hotmail.com>
2023-05-15 21:28:19 +0100

    Solution: Hello

---
+++ 8kyu/hello_.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/56a4addbfd4a55694100001f/train/javascript
+
+function validateHello(greetings) {
+    return /(ahoj|ciao|czesc|h[ae]llo|hola|salut)/i.test(greetings)
+  }
