443150cd896598e855bde91d2fd7be7c8a838356
Bradley Hagger <brad_2nd@hotmail.com>
2023-05-27 19:34:53 +0100

    Solution: Mult Table

---
+++ 6kyu/mult_table.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/534d2f5b5371ecf8d2000a08/train/javascript
+
+multiplicationTable = function(size) {
+    let main_array = []
+    
+    for(let i = 1; i <= size; i++){
+      let array = []  
+      for(let j = 1; j <= size; j++){
+        array.push(i * j)
+      }
+      main_array.push(array)
+    }
+    
+    return main_array
+  }
+  
